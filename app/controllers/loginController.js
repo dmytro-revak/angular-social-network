221db562c060aba36f17953c911d0e0425b84979
dmytro-revak <dmytro27@gmail.com>
2017-04-22 13:39:12 +0300

    add ajax request for users list

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,5 +1,9 @@
-socialNetworkApp.controller('loginController', function () {
+socialNetworkApp.controller('loginController', ['usersService', function (usersService) {
     var login = this;
 
 
-});
+
+    console.log(usersService.usersList);
+
+
+}]);
--- app/services/usersService.js
+++ app/services/usersService.js
@@ -1,5 +1,7 @@
-socialNetworkApp.factory('usersService', function () {
-    return {
-
-    }
+socialNetworkApp.factory('usersService', function ($http) {
+    var usersService = {};
+    $http.get("./users.json").then(function(response) {
+        usersService.usersList = response.data;
+    });
+    return usersService;
 });
