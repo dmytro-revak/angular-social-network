9492475a78ac506f5324d86b0f349d86b63131f9
dmytro-revak <dmytro27@gmail.com>
2017-04-23 13:37:48 +0300

    add filed validation for the register form

--- app/controllers/registerController.js
+++ app/controllers/registerController.js
@@ -1,6 +1,15 @@
-socialNetworkApp.controller('registerController', function () {
+socialNetworkApp.controller('registerController', ['usersService', '$location', function (usersService, $location) {
     vm = this;
 
-    vm.newUser = {};
+    vm.registerUser = function () {
+        usersService.addUser(vm.newUser);
+        vm.newUser = {};
+    };
 
-});
+    vm.returnToLogin = function () {
+        $location.path('/login');
+    };
+
+        // myForm.firstName.$valid
+
+}]);
